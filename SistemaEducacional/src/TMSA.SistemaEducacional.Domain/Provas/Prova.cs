aad0728b3caec7dd6e0397ea5681ed72b49142d0
Thiago Moreira de Souza Arrais <thiagomds.scientist@gmail.com>
2018-08-06 16:42:40 -0300

    Criado a classe prova.cs

--- SistemaEducacional/src/TMSA.SistemaEducacional.Domain/Provas/Prova.cs
+++ SistemaEducacional/src/TMSA.SistemaEducacional.Domain/Provas/Prova.cs
@@ -1,10 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using TMSA.SistemaEducacional.Domain.Core.Models;
 
 namespace TMSA.SistemaEducacional.Domain.Provas
 {
-    public class Prova
+    public class Prova : Entity<Prova>
     {
+        public Prova(
+            Double? notaPrimeiroSemestre,
+            Double? notaSegundoSemestre,
+            Double? mediaDoPrimeiroESegundoSememestre,
+            Double? notaFinal,
+            StatusAprovacao statusAprovacao)
+        {
+            NotaPrimeiroSemestre = notaPrimeiroSemestre;
+            NotaSegundoSemestre = notaSegundoSemestre;
+            MediaDoPrimeiroESegundoSemestre = mediaDoPrimeiroESegundoSememestre;
+            NotaFinal = notaFinal;
+            StatusAprovacao = statusAprovacao;
+        }
+
+        //EF Construtor
+        protected Prova() { }
+
+        public Double? NotaPrimeiroSemestre { get; private set; }
+        public Double? NotaSegundoSemestre { get; private set; }
+        public Double? MediaDoPrimeiroESegundoSemestre { get; private set; }
+        public Double? NotaFinal { get; private set; }
+        public StatusAprovacao StatusAprovacao { get; private set; }
+        public Guid? AlunoId { get; private set; }
+        public Guid? DisciplinaId { get; private set; }
+        public Guid? ProvaId { get; private set; }
+
+
+        public override bool EhValido()
+        {
+            //Todo: 
+            return true;
+        }
     }
 }
---
+++ SistemaEducacional/src/TMSA.SistemaEducacional.Domain/Provas/StatusAprovacao.cs
@@ -0,0 +1,9 @@
+﻿namespace TMSA.SistemaEducacional.Domain.Provas
+{
+    public enum StatusAprovacao
+    {
+        Aprovado,
+        Reprovado,
+        ProvaFinal
+    }
+}
