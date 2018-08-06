1b618e5fb9d9c7721fa2e55f05b7ce50794ce9fa
Thiago Moreira de Souza Arrais <thiagomds.scientist@gmail.com>
2018-08-06 10:05:56 -0300

    Incrementação do código na classe Pessoa.cs

--- SistemaEducacional/src/TMSA.SistemaEducacional.Domain/Pessoas/Pessoa.cs
+++ SistemaEducacional/src/TMSA.SistemaEducacional.Domain/Pessoas/Pessoa.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 using TMSA.SistemaEducacional.Domain.Core.Models;
 using TMSA.SistemaEducacional.Domain.Enderecos;
 
@@ -6,6 +7,23 @@ namespace TMSA.SistemaEducacional.Domain.Pessoas
 {
     public class Pessoa : Entity<Pessoa>
     {
+        public Pessoa(
+            string nome,
+            string email,
+            string ddd,
+            string telefone,
+            string celular)
+        {
+            Id = Guid.NewGuid();
+            Nome = nome;
+            Email = email;
+            DDD = ddd;
+            Telefone = telefone;
+            Celular = celular;
+        }
+
+        // EF Construtor
+        protected Pessoa() { }
 
         public string Nome { get; private set; }
         public string Email { get; private set; }
@@ -19,7 +37,50 @@ namespace TMSA.SistemaEducacional.Domain.Pessoas
 
         public override bool EhValido()
         {
-            throw new NotImplementedException();
+            Validar();
+            return ValidationResult.IsValid;
         }
+
+        #region Validações
+        private void Validar()
+        {
+            ValidarNome();
+            ValidarEmail();
+            ValidarDDD();
+            ValidarTelefone();
+            ValidarCelular();
+            ValidationResult = Validate(this);
+        }
+
+        private void ValidarNome()
+        {
+            RuleFor(c => c.Nome)
+                .NotEmpty().WithMessage("Preencha o nome.");
+        }
+
+        private void ValidarEmail()
+        {
+            RuleFor(c => c.Email)
+                .NotEmpty().WithMessage("Preencha o e-mail.");
+        }
+
+        private void ValidarDDD()
+        {
+            RuleFor(c => c.DDD)
+                .NotEmpty().WithMessage("Preencha o DDD.");
+        }
+
+        private void ValidarTelefone()
+        {
+            RuleFor(c => c.Telefone)
+                .NotEmpty().WithMessage("Preencha o telefone.");
+        }
+
+        private void ValidarCelular()
+        {
+            RuleFor(c => c.Celular)
+                .NotEmpty().WithMessage("Preencha o celular.");
+        }
+        #endregion
     }
 }
