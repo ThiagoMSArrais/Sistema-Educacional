1650c78950505b1f9201441605e3ab2125dd0986
Thiago Moreira de Souza Arrais <thiagomds.scientist@gmail.com>
2018-08-07 10:32:52 -0300

    foi removido o System.Text pois não é utilizado na classe Repository.cs

--- SistemaEducacional/src/TMSA.SistemaEducacional.Infra.Data/Repository/Repository.cs
+++ SistemaEducacional/src/TMSA.SistemaEducacional.Infra.Data/Repository/Repository.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
 using TMSA.SistemaEducacional.Domain.Core.Models;
 using TMSA.SistemaEducacional.Domain.Interfaces;
 using TMSA.SistemaEducacional.Infra.Data.Context;
